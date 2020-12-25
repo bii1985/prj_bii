e9449442c33f265e44495304cf7338cbeb41a9d9
BII <111@yandex.ru>
2020-12-25 09:04:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+"use strict";
+let mymnumber = 5;
+const Mymnumber = {
+    name: 'hgh',
+    age:25,
+    ismar: false
+};
+
+//var name = 'aaa';
+//console.log(mymnumber);
+//console.log(Mymnumber.name);
+//alert(1);
+//const result=confirm('jdhfufd');
+//const answer = +prompt('gcghfg','');
+
+//const answers = [];
+//answers[0] = prompt('bvz','');
+//answers[1] = prompt('dndm','');
+//answers[2] = prompt('ewe','');
+
+//document.write(answers);
