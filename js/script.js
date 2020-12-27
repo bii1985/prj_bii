9351cfe954cc6b6fc8fb31879ad9bce589a9ba68
BII <111@yandex.ru>
2020-12-27 23:21:17 +0300

    start work

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 
 
 "use strict";
-let mymnumber = 5;
+/*let mymnumber = 5;
 const Mymnumber = {
     name: 'hgh',
     age:25,
     ismar: false
-};
+};*/
 
 //var name = 'aaa';
 //console.log(mymnumber);
@@ -15,9 +15,25 @@ const Mymnumber = {
 //const result=confirm('jdhfufd');
 //const answer = +prompt('gcghfg','');
 
-//const answers = [];
-//answers[0] = prompt('bvz','');
-//answers[1] = prompt('dndm','');
-//answers[2] = prompt('ewe','');
 
 //document.write(answers);
+
+let numberOfFilms = +prompt('Сколько фильмов вы просмотрели?','');
+
+const personalMoveDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Последний фильм',''),
+      ab = prompt('Оценка',''),   
+      ac = prompt('Последний фильм',''),
+      ad = prompt('Оценка','');
+
+personalMoveDB.movies[a] = ab;
+personalMoveDB.movies[ac] = ad;
+
+console.log(personalMoveDB);
